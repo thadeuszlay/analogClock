a1cf96cee6066e30229ca067b72395e892275f74
thadeuszlay <thadeuszlay@gmail.com>
2017-02-19 17:46:13 +0100

    calculate seconds, minutes, and hours in degrees and display them

--- analogClock.js
+++ analogClock.js
@@ -1,12 +1,38 @@
 "use strict";
 
-const handSecond = document.querySelector(".clock__hand--second");
-const handMinute = document.querySelector(".clock__hand--minute");
-const handHour = document.querySelector(".clock__hand--hour");
+const handSecond = document.querySelector(".watch__hand--second");
+const handMinute = document.querySelector(".watch__hand--minute");
+const handHour = document.querySelector(".watch__hand--hour");
 
 function updateHands() {
+  const fullCircleDegree = 360;
+  const fullCircleForSeconds = 60;
+  const fullCircleForMinutes = 60;
+  const fullCircleForHours = 12;
+  const degreeOffset = 90;
+
+  const handSecond = document.querySelector('.watch__hand--second');
+
   const now = new Date();
-  console.log("now: ", now);
+  const nowSeconds = now.getSeconds();
+  const nowMinutes = now.getMinutes();
+  const nowHours = now.getHours();
+
+  console.log("nowSeconds: ", nowSeconds);
+
+  const secondsInDegrees = (nowSeconds / fullCircleForSeconds) *
+    fullCircleDegree;
+  const minutesInDegrees = (nowMinutes / fullCircleForMinutes) *
+    fullCircleDegree;
+  const hoursInDegrees = (nowHours / fullCircleForHours) *
+    fullCircleDegree;
+  console.log("secondsInDegrees: ", secondsInDegrees);
+
+  handSecond.style.transform = `rotate(${secondsInDegrees - degreeOffset}deg)`;
+  handMinute.style.transform = `rotate(${minutesInDegrees - degreeOffset}deg)`;
+  handHour.style.transform = `rotate(${hoursInDegrees - degreeOffset}deg)`;
+
+
 }
 
-updateHands();
+setInterval(updateHands, 1000);
