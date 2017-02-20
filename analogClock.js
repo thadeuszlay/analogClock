f0be91806346d6e3f5e5b75e77c24102a8aec125
thadeuszlay <thadeuszlay@gmail.com>
2017-02-20 11:55:46 +0100

    reordering of variables

--- analogClock.js
+++ analogClock.js
@@ -1,34 +1,40 @@
 "use strict";
 
+const fullCircleDegree = 360;
+const fullCircleForSeconds = 60;
+const fullCircleForMinutes = 60;
+const fullCircleForHours = 12;
+const degreeOffset = 90;
+
 const handSecond = document.querySelector(".watch__hand--second");
 const handMinute = document.querySelector(".watch__hand--minute");
 const handHour = document.querySelector(".watch__hand--hour");
 
+let now = null;
+let nowSeconds = null;
+let nowMinutes = null;
+let nowHours = null;
+
+let secondsInDegrees = null;
+let minutesInDegrees = null;
+let hoursInDegrees = null;
+
 function updateHands() {
-  const fullCircleDegree = 360;
-  const fullCircleForSeconds = 60;
-  const fullCircleForMinutes = 60;
-  const fullCircleForHours = 12;
-  const degreeOffset = 90;
+  now = new Date();
+  nowSeconds = now.getSeconds();
+  nowMinutes = now.getMinutes();
+  nowHours = now.getHours();
 
-  const handSecond = document.querySelector('.watch__hand--second');
-
-  const now = new Date();
-  const nowSeconds = now.getSeconds();
-  const nowMinutes = now.getMinutes();
-  const nowHours = now.getHours();
-
-  const secondsInDegrees = ((nowSeconds / fullCircleForSeconds) *
-    fullCircleDegree) - degreeOffset;
-  const minutesInDegrees = ((nowMinutes / fullCircleForMinutes) *
-    fullCircleDegree) - degreeOffset;
-  const hoursInDegrees = ((nowHours / fullCircleForHours) *
-    fullCircleDegree) - degreeOffset;
+  secondsInDegrees = ((nowSeconds / fullCircleForSeconds) * fullCircleDegree) -
+    degreeOffset;
+  minutesInDegrees = ((nowMinutes / fullCircleForMinutes) * fullCircleDegree) -
+    degreeOffset;
+  hoursInDegrees = ((nowHours / fullCircleForHours) * fullCircleDegree) -
+    degreeOffset;
 
   handSecond.style.transform = `rotate(${secondsInDegrees}deg)`;
   handMinute.style.transform = `rotate(${minutesInDegrees}deg)`;
   handHour.style.transform = `rotate(${hoursInDegrees}deg)`;
-
 }
 
 setInterval(updateHands, 1000);
